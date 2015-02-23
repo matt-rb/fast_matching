609a9a0d5e028fd3399eb1a4d58386ee98c624a3
Mahdyar Ravanbakhsh <mahdyar.ravanbakhsh@comland.si>
2015-02-23 21:17:21 +0100

    add mute to ITQ

--- tools/make_hash_table.m
+++ tools/make_hash_table.m
@@ -1,12 +1,20 @@
-function [ hash_table ] = make_hash_table( bin_vect, orig_vecotr, kps)
+function [ hash_table ] = make_hash_table( bin_vect, orig_vecotr, kps, mute)
 %UNTITLED2 Summary of this function goes here
 %   Detailed explanation goes here
+    if ~exist('mute','var')
+        mute=false;
+    end
+    
     hash_size = bi2de(ones(1,size(bin_vect,2)),'left-msb')+1;
     hash_table = cell(hash_size,2);
-    fprintf(1,'Claculate Hash Table (size %d) current: ',hash_size);
+    if ~mute
+        fprintf(1,'Claculate Hash Table (size %d) current: ',hash_size);
+    end
     for i=1:hash_size
-        reverseStr = repmat(sprintf('\b'), 1, length(num2str(i-1)));
-        fprintf(1,strcat(reverseStr,'%d'),i);
+        if ~mute
+            reverseStr = repmat(sprintf('\b'), 1, length(num2str(i-1)));
+            fprintf(1,strcat(reverseStr,'%d'),i);
+        end;
         idx_binaries = find (bi2de(bin_vect(:,:),'left-msb')==(i-1));
         hash_table(i,1) = {orig_vecotr(idx_binaries,:)};
         hash_table(i,2) = {kps(idx_binaries,:)};
--- tools/train_itq.m
+++ tools/train_itq.m
@@ -1,17 +1,28 @@
-function [ itq_bin_mat,itq_rot_mat,pca_mapping, mean_data ] = train_itq( pca_size, n_iter, temp_features )
+function [ itq_bin_mat,itq_rot_mat,pca_mapping, mean_data ] = train_itq( pca_size, n_iter, temp_features ,mute)
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
-
-    fprintf('Normalize Data...\n');
+    
+    if ~exist('mute','var')
+        mute=false;
+    end
+    if ~mute
+        fprintf('Normalize Data...\n');
+    end
     [temp_features, mean_data] = normalize_features( temp_features );
 
     %----- PCA ---------
-    fprintf('Computing Cov PCA...\n');
+    if ~mute
+        fprintf('Computing Cov PCA...\n');
+    end
     Cov=temp_features'*temp_features;
-    fprintf('Computing Mapping PCA...\n');
+    if ~mute
+        fprintf('Computing Mapping PCA...\n');
+    end
     [pca_mapping,~]=eigs(Cov,pca_size);
     mappeddata = temp_features * pca_mapping;
-    fprintf('Computing ITQ...\n');
+    if ~mute
+        fprintf('Computing ITQ...\n');
+    end
     [itq_bin_mat,itq_rot_mat] = ITQ(mappeddata, n_iter);
 end
 
